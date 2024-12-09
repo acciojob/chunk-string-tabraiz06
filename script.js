70a0c0394eaeb87cb7658360509f958c56925eed
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-12-09 11:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,4 +15,6 @@ function chunkString(str, chunkLength) {
 // Do not change the code below 
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
+
 alert(chunkString(str, size));
+
